5fa44c7408fcf6982c2495cfda5a314d4acfba3f
Irene Lai <irenelai.pr@gmail.com>
2021-03-09 13:37:18 +0800

    Search Function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 const mat_modules = [
   MatAutocompleteModule,
@@ -35,7 +36,8 @@ const mat_modules = [
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
